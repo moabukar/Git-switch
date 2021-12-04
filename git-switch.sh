bcf92880d21f16be1879f59b8e7a53649e4769b4
moabukar <mo-abukar10@hotmail.com>
2021-12-04 14:36:38 +0000

    new function + select user

--- git-switch.sh
+++ git-switch.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-// script here
+gitaccounts="moabukar"
+# first
+declare -A github
+github[moabukar]='personal'
+
+
+# git config function
+function gitconfig_switch() {
+        USER=$1
+        GIT_URL=$2
+        REMOTE=$3
+        sed -r -i "/remote \"$REMOTE\"/,+2s/(.*)+@$GIT_URL/\turl = https:\/\/$USER@$GIT_URL/" .git/config
+}
+
+select USER IN $gitaccounts; do
+        gitconfig_switch origin $REPO $USER
+
+        GITHUB_USER=${github[$USER]}
+        if [ ! -z "$GITHUB_USER" ]; then
+                gitconfig_switch github github.com $GITHUB_USER
+
+        fi;
+    
+    break
+
+done
